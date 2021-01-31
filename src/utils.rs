a1f091c29c17a50e01612b459311a86a4f2212de
Danilo Guanabara <danguafer@gmail.com>
2021-01-30 22:40:27 -0300

    Checking if crate is publishable

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use serde::de::DeserializeOwned;
 use std::process::Command;
 
@@ -8,6 +8,24 @@ pub fn get<T: DeserializeOwned>(url: &str) -> serde_json::Result<T> {
     let mut body = String::new();
     res.read_to_string(&mut body).expect("Couldn't read body to string.");
     serde_json::from_str(&body)
+}
+
+fn execute_with_output(command: &str, args: &[&str]) -> Result<String, String> {
+    println!("Executing: {} {:?}", command, args);
+    let output = Command::new(command)
+        .args(args.iter())
+        .output()
+        .expect("Couldn't get Output.");
+
+    if output.status.success() {
+        std::io::stdout().write_all(&output.stdout).unwrap();
+        std::io::stderr().write_all(&output.stderr).unwrap();
+        let stderr = String::from_utf8_lossy(&output.stderr);
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        Ok(format!("{}\n{}", stderr, stdout))
+    } else {
+        Err(format!("{} {:?}: execution failed", command, args))
+    }
 }
 
 fn execute(command: &str, args: &[&str]) -> Result<(), String> {
@@ -32,5 +50,6 @@ pub fn publish(release: &str, cargo_token: &str) -> Result<(), String> {
 }
 
 pub fn check_publish() -> Result<(), String> {
-    execute("cargo", &["publish", "--dry-run"])
+    let output = execute_with_output("cargo", &["publish", "--dry-run"])?;
+    output.find("warning").ok_or_else(|| "Can't publish crate.".to_string()).map(|_| ())
 }
